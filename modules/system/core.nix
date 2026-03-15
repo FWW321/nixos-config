78cd3424eb96908705b97673bb1917b39ffa5956
fww <3223400498@qq.com>
2026-03-15 18:09:43 +0800

    fix:cs2 network

--- modules/system/core.nix
+++ modules/system/core.nix
@@ -82,7 +82,6 @@
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
-    gamescopeSession.enable = true;
     extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
 
